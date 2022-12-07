7dd44a5caccf04b0456d34d151deaecb0b2e5542
stodoriko <s.todorikos@gmail.com>
2022-12-07 17:22:58 +0300

    puppeteer homework

---
+++ gh.test.js
@@ -0,0 +1,37 @@
+let page;
+
+afterEach(async() => {
+   await page.close();
+ });
+
+describe("Github page tests", () => {
+  beforeEach(async () => {
+   page = await browser.newPage();
+   await page.goto("https://github.com/team");
+});
+
+  test("The h1 header content'", async () => {
+    await page.setDefaultTimeout(3000);
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+  });
+
+  test("The first link attribute", async () => {
+    await page.setDefaultTimeout(2000);
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  });
+
+  test("The page contains Sign in button", async () => {
+    await page.setDefaultTimeout(2000);
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Sign up for free")
+  });
+});
